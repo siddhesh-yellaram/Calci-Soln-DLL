5216eda0bd6e8fb072de504554d4caa8ac7b51a7
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 01:05:34 +0530

    Add files via upload

---
+++ CalciLib/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalciLib
+{
+    public class Calculator
+    { 
+        public bool CheckEven(int num)
+        {
+            if (num % 2 == 0)
+                return true;
+            return false;
+        }
+
+        public bool CheckPrime(int num)
+        {
+            // Check if num is less than or equal to 1
+            if (num <= 1)
+                return false;
+
+            // Check if num is 2
+            else if (num == 2)
+                return true;
+
+            // Check if num is a multiple of 2
+            else if (num % 2 == 0)
+                return false;
+
+            //Check for odd nums
+            for (int i = 3; i <= Math.Sqrt(num); i += 2)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ CalciLibTest/CalciTest.cs
@@ -0,0 +1,89 @@
+﻿using CalciLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalciLibTest
+{
+    [TestClass]
+    public class CalciTest
+    {
+        Calculator c = new Calculator();
+
+        [TestMethod]
+        public void checkNo2IsEven()
+        {    
+            bool expectedValue = true;
+            int inputValue = 2;
+
+            bool actualValue = c.CheckEven(inputValue);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void checkNo0IsEven()
+        {
+            bool expectedValue = true;
+            int inputValue = 0;
+
+            bool actualValue = c.CheckEven(inputValue);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void checkNoNeg1IsEven()
+        {
+            bool expectedValue = false;
+            int inputValue = -1;
+
+            bool actualValue = c.CheckEven(inputValue);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void checkNo1IsPrime()
+        {
+            bool expectedValue = false;
+            int inputValue = 1;
+
+            bool actualValue = c.CheckPrime(inputValue);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void checkNoNeg1IsPrime()
+        {
+            bool expectedValue = false;
+            int inputValue = -1;
+
+            bool actualValue = c.CheckPrime(inputValue);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void checkNo0IsPrime()
+        {
+            bool expectedValue = false;
+            int inputValue = 0;
+
+            bool actualValue = c.CheckPrime(inputValue);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod]
+        public void checkNo3IsPrime()
+        {
+            bool expectedValue = true;
+            int inputValue = 3;
+
+            bool actualValue = c.CheckPrime(inputValue);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
